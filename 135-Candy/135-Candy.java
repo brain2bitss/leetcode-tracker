4dcee4981548953649187fdd75f32b190a544463
brain2bitss <brain2bitss@gmail.com>
2026-02-02 04:04:42 -0500

    Create 135-Candy

---
+++ 135-Candy/135-Candy.java
@@ -0,0 +1,31 @@
+// Last updated: 2/2/2026, 2:34:42 PM
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        
+        // Step 1: Give each child at least 1 candy
+        Arrays.fill(candies, 1);
+
+        // Step 2: Left-to-right pass
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Step 3: Right-to-left pass
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Step 4: Sum up candies
+        int total = 0;
+        for (int c : candies) {
+            total += c;
+        }
+        return total;
+    }
+}
